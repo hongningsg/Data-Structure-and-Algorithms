a3fcdcd64e7f62aee639393907c26b4d3648466d
Hongning Shangguan <hongningsg@yahoo.com>
2019-03-06 20:48:43 +1100

    implement quicksort and heapsort

---
+++ Sorting/HeapSort.java
@@ -0,0 +1,43 @@
+//HeapSort takes O(nlogn) time and O(1) space
+//Build max heap and extract root element one by one with keeping it max heap!
+public class HeapSort{
+      public void sort(int[] arr){
+            buildMaxHeap(arr);
+            extract(arr);
+      }
+
+      private int parentOf(int i) { return (i - 1) / 2;}
+      private int left(int i) { return i * 2 + 1;}
+      private int right(int i) { return i * 2 + 2;}
+      private void swap(int[] arr, int i, int j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+      }
+      private void maxHeapify(int[] arr, int i){
+            int l = left(i);
+            int r = right(i);
+            int root = i;
+            if (l < arr.length && arr[l] > arr[root]){
+                  root = l;
+            }
+            if (r < arr.length && arr[r] > arr[root]){
+                  root = r;
+            }
+            if (root != i){
+                swap(arr, i, root);
+                maxHeapify(arr, root);
+            }
+      }
+      private void buildMaxHeap(int arr[]){
+            for ( int i = parentOf(arr.length - 1); i > -1; i--){
+                  maxHeapify(i);
+            }
+      }
+      private void extract(int arr[]){
+            for (int i = arr.length - 1; i > 0; i--) {
+                  swap(arr, i, 0);
+                  maxHeapify(arr, 0);
+            }
+      }
+}
---
+++ Sorting/QuickSort.java
@@ -0,0 +1,34 @@
+//QuickSort in O(nlogn) avg time (O(n) in best, O(n^2) in worst case) and O(1) space
+//choose pivot randomly
+import java.util.Arrays;
+import java.util.Random;
+public class QuickSort{
+      public void sort(int[] arr){
+          quicksort(arr, 0, arr.length - 1);
+      }
+
+      private void quicksort(int[] arr, int low, int high){
+            if (low < high){
+                  int p = partition(arr, low, high);
+                  quicksort(arr, low, --p);
+                  quicksort(arr, ++p, high);
+            }
+      }
+
+      private void swap(int[] arr, int i, int j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+      }
+
+      private int partition(int[] arr, int low, int high){
+            int pivot = Random().nextInt(high - low + 1) + low;
+            swap(arr, low, pivot);
+            pivot = low++;
+            for (int i = low; i <= high; i++) {
+                  if (arr[i] < arr[pivot]) swap(arr, i, low++);
+            }
+            swap(arr, pivot, --low);
+            return low;
+      }
+}

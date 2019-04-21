dc1d8db4439eb12639e39bbf6c089616c153d921
Hongning Shangguan <hongningsg@yahoo.com>
2019-04-21 21:41:21 +1000

    RBsearch

--- Tree/RBTree.java
+++ Tree/RBTree.java
@@ -1,1 +1,26 @@
 package Tree;
+public class RBTree<T extends Comparable<T>>{
+  private RBTreeNode<T> root;
+  private int size;
+
+  public RBTree(){
+    this.root = new RBTreeNode(null);
+  }
+
+  public T search(T value){
+    RBTreeNode<T> curr = this.root;
+    while (curr != null){
+      int compare_val = curr.value.compareTo(value);
+      if (compare_val < 0) {
+        curr = curr.right;
+      }else if(compare_val > 0){
+        curr = curr.left;
+      }else{
+        return curr.value;
+      }
+    }
+    return null;
+  }
+
+  
+}

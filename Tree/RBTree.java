d3b5e942d951fa80c4e8fb6f84aaff508e34ab91
Hongning Shangguan <hongningsg@yahoo.com>
2019-04-22 14:35:10 +1000

    insert rbt

--- Tree/RBTree.java
+++ Tree/RBTree.java
@@ -2,6 +2,8 @@ package Tree;
 public class RBTree<T extends Comparable<T>>{
   private RBTreeNode<T> root;
   private int size;
+  private final boolean black = false;
+  private final boolean red = true;
 
   public RBTree(){
     this.root = new RBTreeNode(null);
@@ -22,5 +24,19 @@ public class RBTree<T extends Comparable<T>>{
     return null;
   }
 
-  
+  private RBTreeNode add(RBTreeNode curr,T key){
+    if (curr == null) {
+      this.size++;
+      RBTreeNode newNode = new RBTreeNode(key, red);
+      return newNode;
+    }
+    int compare_val = curr.value.compareTo(value);
+    if (compare_val < 0) {
+      curr.right = this.add(curr.right, key);
+    }else if (compare_val > 0){
+      curr.left = this.add(curr.left, key);
+    }else{
+      return curr;
+    }
+  }
 }

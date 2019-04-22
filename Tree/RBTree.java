eb13f45d3a3e88f80ff4bef188972e35380dc856
Hongning Shangguan <hongningsg@yahoo.com>
2019-04-22 16:36:37 +1000

    operations

--- Tree/RBTree.java
+++ Tree/RBTree.java
@@ -9,6 +9,50 @@ public class RBTree<T extends Comparable<T>>{
     this.root = new RBTreeNode(null);
   }
 
+  private RBTreeNode<T> grandParent(RBTreeNode<T> node){
+    RBTreeNode<T> parent = node.parent;
+    if (parent != null){
+      return parent.parent;
+    }
+    return null;
+  }
+
+  private RBTreeNode<T> sibling(RBTreeNode<T> node){
+    RBTreeNode<T> parent = node.parent;
+    if (parent == null) return null;
+    return node == parent.left? parent.right: parent.left;
+  }
+
+  private RBTreeNode<T> uncle(RBTreeNode<T> node){
+    RBTreeNode<T> grandParent = this.grandParent(node);
+    if (grandParent == null) {
+      return null;
+    }
+    return this.sibling(node.parent);
+  }
+
+  private RBTreeNode<T> rotateLeft(RBTreeNode<T> node){
+    RBTreeNode<T> new_root = node.right;
+    node.right = new_root.left;
+    new_root.left = node;
+    node.parent = new_root;
+    if (node.right != null) {
+      node.right.parent = node;
+    }
+    return new_root;
+  }
+
+  private RBTreeNode<T> rotateRight(RBTreeNode<T> node){
+    RBTreeNode<T> new_root = node.left;
+    node.left = new_root.right;
+    new_root.right = node;
+    node.parent = new_root;
+    if (node.left != null) {
+      node.left.parent = node;l
+    }
+    return new_root;
+  }
+
   public T search(T value){
     RBTreeNode<T> curr = this.root;
     while (curr != null){
@@ -24,7 +68,11 @@ public class RBTree<T extends Comparable<T>>{
     return null;
   }
 
-  private RBTreeNode add(RBTreeNode curr,T key){
+  public void insert(T key){
+    this.add(this.root, key);
+  }
+
+  private RBTreeNode<T> add(RBTreeNode curr,T key){
     if (curr == null) {
       this.size++;
       RBTreeNode newNode = new RBTreeNode(key, red);
@@ -33,10 +81,17 @@ public class RBTree<T extends Comparable<T>>{
     int compare_val = curr.value.compareTo(value);
     if (compare_val < 0) {
       curr.right = this.add(curr.right, key);
+      curr.right.parent = curr;
     }else if (compare_val > 0){
       curr.left = this.add(curr.left, key);
+      curr.left.parent = curr;
     }else{
       return curr;
     }
+    insertFix(curr);
+  }
+
+  private insertFix(RBTreeNode<T> node){
+
   }
 }

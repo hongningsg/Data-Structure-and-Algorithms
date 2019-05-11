fa6e786fb99b62ff83c538f56c8eb2ec82687e1f
Hongning Shangguan <hongningsg@yahoo.com>
2019-05-11 20:11:53 +1000

    B+ Tree create

---
+++ Tree/B_plusTree.java
@@ -0,0 +1,13 @@
+//just a simple binary tree node
+package Tree;
+public class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int v){
+        val = v;
+        left = null;
+        right = null;
+    }
+}
